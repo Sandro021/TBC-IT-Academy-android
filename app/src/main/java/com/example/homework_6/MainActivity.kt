b8c8184020985e8d46b01476662df7246bf69cef
Sandro Mtchedlidze <s.mchedlidze2004@gmail.com>
2025-10-08 01:45:11 +0400

    Final commit

---
+++ app/src/main/java/com/example/homework_6/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.homework_6
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.homework_6.databinding.ActivityMainBinding
+import com.example.homework_6.ui.theme.Homework6Theme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Homework6Theme {
+        Greeting("Android")
+    }
+}
