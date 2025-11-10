1af9602ede0fe388150523f1390b33d1562c2046
Sandro Mtchedlidze <s.mchedlidze2004@gmail.com>
2025-11-10 15:42:20 +0400

    Final commit

---
+++ app/src/main/java/com/example/homework_18/screen/LoginFragment.kt
@@ -0,0 +1,15 @@
+package com.example.homework_18.screen
+
+import com.example.homework_18.common.BaseFragment
+import com.example.homework_18.databinding.FragmentLoginBinding
+
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+    override fun bind() {
+        TODO("Not yet implemented")
+    }
+
+    override fun listeners() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/homework_18/screen/WelcomeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.homework_18.screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.homework_18.R
+import com.example.homework_18.common.BaseFragment
+import com.example.homework_18.databinding.FragmentWelcomeBinding
+
+
+class WelcomeFragment : BaseFragment<FragmentWelcomeBinding>(FragmentWelcomeBinding::inflate) {
+    override fun bind() {
+        TODO("Not yet implemented")
+    }
+
+    override fun listeners() {
+        TODO("Not yet implemented")
+    }
+
+}
